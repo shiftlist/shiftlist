5307c24e87723b4c7360a7c3b51e839365395c24
conqsolid <conqsolid@gmail.com>
2019-11-30 17:09:15 +0300

    bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
+import { Button, Form, FormControl, Nav, Navbar, NavDropdown, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from "react-router-dom";
 import './App.css';
-
-import logo from './logo.svg';
+import Home from './Pages/Home';
+import TimeSheet from './Pages/TimeSheet';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">TimeSheet App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/timesheet">TimeSheet</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="content">
+          <Switch>
+            <Route exact={true} path='/' component={Home} />
+            <Route path='/timesheet' component={TimeSheet} />
+          </Switch>
+        </div>
+      </Router>
+
     );
   }
 }
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+const Home: React.FC = (props) => {
+    return (
+        <div>
+            Home
+        </div >
+    )
+}
+export default withRouter(Home);
